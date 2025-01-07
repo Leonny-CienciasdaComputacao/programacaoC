f1bdf38cb74997736c9413deefc23aa2b477b862
Leonny-CienciasdaComputacao <leobslopes@gmail.com>
2025-01-06 21:59:52 -0300

    Update teste.c

--- teeste.c
+++ teeste.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main () {
+  char [20] nome;
+  float idade;
+  double salario;
+    nome = 'alice';
+    idade = 25;
+    salario = 4500.55
+
+  printf ("%s %f %f", nome, idade, salario)
+
+    return 0;
+
+
+}
